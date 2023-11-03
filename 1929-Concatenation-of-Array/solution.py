cd7481314804e6ccf846961a684d0da6a8b4a980
rvDOOM <rvalerio9131@gmail.com>
2023-11-03 14:27:25 -0400

    Restructure

--- 1929-Concatenation-of-Array/solution.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def getConcatenation(self, nums: List[int]) -> List[int]:
-        ans = []
-
-        for i in range(2):
-            for j in range(len(nums)):
-                ans.append(nums[j])
-
-        return ans
